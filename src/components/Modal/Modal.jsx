453ad9daf92acafc903711c762459bb0b36e6d19
harsimran-increasingly <harsimran@increasingly.com>
2022-10-18 17:26:20 +0530

    added modal and eslint prop type checking with airbnb style guides

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Modal  from 'react-modal';
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+function ModalPopup() {
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+   
+    return (
+        <div> <div>
+            <button className='inc_product_description_button' onClick={()=> openModal()}>Select Option</button>
+            <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+              
+                <button onClick={closeModal}>close</button>
+                
+            </Modal>
+        </div></div>
+    )
+}
+
+export default ModalPopup
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types';
 import { formatter } from '../../../lib/helpers'
 import useStore from '../../../store/store'
 import './Product.scss'
+import ModalPopup from '../Modal/Modal';
+
+function ActionButton({ isPresent, performAction, ProductType }) {
+    if (ProductType === "simple") {
+        return (<button type='button' className={isPresent ? " inc_product_description_button inc_product_description_button_added" :
+            "inc_product_description_button"} onClick={() => performAction()}>{isPresent ? "Added" : "Add To Cart"}</button>)
+    }
+    return <ModalPopup />
+}
 function Product(props) {
-    const { ProductName, ImageURL, Price,ProductId } = props
+
+    const { ProductName, ImageURL, Price, ProductId, ProductType } = props
     const addToStore = useStore((state) => state.addToStore)
     const removeFromStore = useStore((store) => store.removeFromStore)
     const addedProductIds = useStore((store) => store.addedProductIds)
@@ -16,24 +27,39 @@ function Product(props) {
             addToStore(props)
         }
     }
+
+
     useEffect(() => {
         let check = addedProductIds.find((item) => item === ProductId)
         setIsPresent(check)
     }, [addedProductIds])
+
     return (
         <div className='inc_product_block'>
             <div className="inc_product_image">
-                <img className="inc_product_image_element" src={ImageURL} />
+                <img className="inc_product_image_element" src={ImageURL} alt={ProductName} />
             </div>
             <div className="inc_product_description">
                 <div className="inc_product_description_name">{ProductName}</div>
                 <div className="inc_product_description_price">{formatter.format(Price)}</div>
-                <div className={ isPresent ? " inc_product_description_button inc_product_description_button_added" : 
-            "inc_product_description_button"}  onClick={() => performAction()}>{isPresent ? "Added" : "Add To Cart"}</div>
+                <ActionButton ProductType={ProductType} isPresent={isPresent} performAction={performAction} />
             </div>
-
         </div>
     )
 }
 
+Product.propTypes = {
+    ProductName: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string.isRequired,
+    Price: PropTypes.string.isRequired,
+    ProductType: PropTypes.string.isRequired,
+    ProductId: PropTypes.string.isRequired,
+
+}
+
+ActionButton.propTypes = {
+    isPresent: PropTypes.bool.isRequired,
+    performAction: PropTypes.func.isRequired,
+    ProductType: PropTypes.string.isRequired
+}
 export default Product
